8981328cdfd7d57fa9cb06b57997921d37042caa
R0g3r <rogegolbe@gmail.com>
2025-07-03 06:55:08 -0500

    GET, DELETE, PUT in events

--- CRUD-OMR/backend/app.py
+++ CRUD-OMR/backend/app.py
@@ -22,16 +22,55 @@ def format_event(event):
         "id":event.id,
         "created_at": event.created_at
     }
+
+
 app.route('/')
 def hello():
     return 'Hey!'
-app.route('/event', methods = ['POST'])
+
+# create an event
+app.route('/events', methods = ['POST'])
 def create_event():
     description = request.json['description']
     event = Event(description)
     db.session.add(event)
     db.session.commit()
+    return format_event(event)
+
+# get all events  
+app.route('/events', methods=['GET'])
+def get_events():
+    events = Event.query.order_buf(Event.id.asc()).all()
+    event_list = []
+    for event in events:
+        event_list.append(format_event(event))
+    return {"events": event_list}
+
+# get an event by id
+app.route('/events/<id>', methods=['GET'])
+def get_event(id):
+    event = Event.filter_by(id=id).one()
+    formatted_event = format_event(event)
+    return {"event": formatted_event}
+
+# delete an event by id
+app.route('/events/<id>', methods=['DELETE'])
+def delete_event(id):
+    event = Event.query.filter_by(id=id).one()
+    db.session.delete(event)
+    db.session.commit()
+    return {"message": "Event (ID: {id}) deleted successfully"}
+
+# update an event by id
+app.route('/events/<id>', methods=['PUT'])
+def update_event(id):
+    event = Event.query.filter_by(id=id)
+    description = request.json['description']
+    event.update(dict(description=description, created_at=datetime.utcnow()))
+    db.session.commit()
+    return {'event': format_event(event.one())}
     
-    
+
 if __name__ == '__main__':
     app.run()
+
